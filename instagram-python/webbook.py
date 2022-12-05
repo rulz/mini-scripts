bd42c1332a335bc0f9fb56238bd0cadfc4cddfd5
Raúl Sepúlveda <raulsetron@gmail.com>
2022-12-04 22:42:10 -0300

    requerimientos

--- instagram-python/webbook.py
+++ instagram-python/webbook.py
@@ -1,15 +1,29 @@
-from instabot import Bot
-import os
+## funciona bien
+from instagrapi import Client
+from instagrapi.types import Usertag, Location
 
-os.system("rm -rf config")
-bot  = Bot()
-bot.login(username='elrulz', password='Elrulz1212!')
+import threading
 
-# image = 'img/img-bot.jpeg'
-# bot.upload_photo(image, caption="Test 1")
-dir_image = 'img/'
+cl = Client()
+cl.login('elrulz', 'Elrulz1212!')
+# user = cl.user_info_by_username('rulzcl')
 
-count = 1
-for image in os.listdir(dir_image):
-    bot.upload_photo(dir_image + image, caption="Prueba {}".format(count))
-    count += 1
+for i in range(1, 4):
+    path = '/Users/raulsepulveda/Proyectos/TIClass/instagram-python/img/e-test-1.jpeg'
+    caption = 'FF Prueba {}'.format(i)
+    extra_data = {
+        'custom_accessibility_caption': 'alt FF prueba {}'.format(i),
+        'like_and_view_counts_disabled': False,
+        'disable_comments':False,
+    }
+    medio = cl.photo_upload(
+        path=path,
+        caption=caption,
+        extra_data=extra_data
+    )
+
+    # process = threading.Thread(
+    #     target=cl.photo_upload,
+    #     args=[path, caption, extra_data]
+    # )
+    # process.start()
